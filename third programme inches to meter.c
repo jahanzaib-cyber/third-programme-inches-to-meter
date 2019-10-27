9868894371ec3ffa0f9d88ec8e31f80190b12d58
agha <56580762+jahanzaib-cyber@users.noreply.github.com>
2019-10-27 11:59:24 -0700

    Add files via upload

---
+++ third programme inches to meter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+		int inch;
+		float result;
+		printf("Enter the value of inch:");f
+		scanf("%d",&inch);
+		result=inch/0.0254,"%f",&result;
+		printf("result=%f",result);
+		return 0;
+}
